b45afb8b6e5d65e80b8dce85d9fe11678e2570eb
Hebatullah Dwairi <hebamahmoud8843@gmail.com>
2025-02-04 16:30:32 +0300

    3.3

--- index.js
+++ index.js
@@ -36,6 +36,17 @@ app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${count} people <br/> ${time}`);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    const person = persons.find(person => person.id === id);
+
+    if(person){
+        response.json(person);
+    }
+    else{
+        response.status(404).end();
+    }
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
