d2f62c9e5420e2549e1a3cdcc4b47f4bd56e397d
Hebatullah Dwairi <hebamahmoud8843@gmail.com>
2025-02-08 02:00:28 +0300

    .

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 
@@ -85,7 +88,7 @@ app.post('/api/persons', (request, response) => {
     response.json(newPerson);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
 });
